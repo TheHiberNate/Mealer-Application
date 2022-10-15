7ee8fe85227b59672adc59e85ec55d4e49b87012
TheHiberNate <nathan@gawargy.com>
2022-10-15 16:33:19 -0400

    Added Firebase, RegisterPage and Modifications

--- app/src/main/java/com/example/mealer/MainActivity.java
+++ app/src/main/java/com/example/mealer/MainActivity.java
@@ -3,14 +3,32 @@ package com.example.mealer;
 
 // Test
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (Button) findViewById(R.id.btn_Register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_Register:
+                startActivity(new Intent(this, register_page.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/mealer/register_page.java
@@ -0,0 +1,13 @@
+package com.example.mealer;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class register_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page);
+    }
+}
