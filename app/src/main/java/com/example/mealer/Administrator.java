f73c6a3a58bf359efebb33f82056f1ffee6a4b37
TheHiberNate <nathan@gawargy.com>
2022-10-18 17:13:09 -0400

    Changed Class Constructors

--- app/src/main/java/com/example/mealer/Administrator.java
+++ app/src/main/java/com/example/mealer/Administrator.java
@@ -2,7 +2,6 @@ package com.example.mealer;
 
 public class Administrator extends User {
     public Administrator(String firstName, String lastName, String email, String address) {
-        super(firstName, lastName, email, address);
-        setRole("Administrator");
+        super(firstName, lastName, email, address, "Administrator");
     }
 }
--- app/src/main/java/com/example/mealer/Client.java
+++ app/src/main/java/com/example/mealer/Client.java
@@ -2,7 +2,6 @@ package com.example.mealer;
 
 public class Client extends User{
     public Client(String firstName, String lastName, String email, String address) {
-        super(firstName, lastName, email, address);
-        setRole("Client");
+        super(firstName, lastName, email, address, "Client");
     }
 }
