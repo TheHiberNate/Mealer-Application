70daa6eed57842aada161e77d2dcf7e2348291df
cherguihakim <cherguihakim7@gmail.com>
2022-10-17 20:19:44 -0400

    Main without renderproblem

--- app/src/main/java/com/example/mealer/Home_page.java
+++ app/src/main/java/com/example/mealer/Home_page.java
@@ -1,8 +1,21 @@
 package com.example.mealer;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home_page extends AppCompatActivity {
 
@@ -11,4 +24,5 @@ public class Home_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
     }
+
 }
