cd52e02ceba3433f3048527a4f8101d40ded915b
TheHiberNate <nathan@gawargy.com>
2022-10-18 21:12:40 -0400

    Added Public Methods For Private Variables and Updates for XML file

--- app/src/main/java/com/example/mealer/User.java
+++ app/src/main/java/com/example/mealer/User.java
@@ -8,6 +8,7 @@ public class User {
     private String role;
     //private String address;
 
+    public User(){}
 
     public User(String firstName, String lastName, String email, String address, String role) {
         this.firstName = firstName;
@@ -17,5 +18,44 @@ public class User {
         this.role = role;
         //this.address = address;
     }
-    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
