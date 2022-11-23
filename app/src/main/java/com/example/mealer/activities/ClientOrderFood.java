0b182f2f05cbb270fea80b7da5746503b80e5609
TheHiberNate <nathan@gawargy.com>
2022-11-23 16:36:29 -0500

    Update ClientOrderFood.java

--- app/src/main/java/com/example/mealer/activities/ClientOrderFood.java
+++ app/src/main/java/com/example/mealer/activities/ClientOrderFood.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -31,13 +32,14 @@ import java.util.ArrayList;
 
 public class ClientOrderFood extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
     private String[] categories = {"No Filter", "Chef", "Meal", "Vegetarian"};
-    private String searchFilter, mealID;
+    private String searchFilter, mealID, chefID;
     private Spinner options;
     private EditText searchEditText;
     private TextView noMeals;
     private ListView searchResultsListView;
     private Button searchBtn, backBtn;
     private ArrayList<Meal> mealList;
+    private ArrayList<Chef> chefList;
     private ArrayList<String> listMealID, listChefID;
     private Meal meal;
     private Chef chef;
@@ -84,39 +86,6 @@ public class ClientOrderFood extends AppCompatActivity implements AdapterView.On
     @Override
     protected void onStart() {
         super.onStart();
-        searchFilter = searchEditText.getText().toString();
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                mealList.clear();
-                listMealID.clear();
-                listChefID.clear();
-                for (DataSnapshot ds : snapshot.getChildren()) {
-                    if (ds.child("role").getValue().equals("Chef") && ds.child("suspended").getValue().equals(false)) {
-                        // No Filter search criteria
-                        if (searchFilter.equals(categories[0])) {
-                            DataSnapshot meals = ds.child("menu").child("meals");
-                            for (DataSnapshot n : meals.getChildren()) {
-                                if (!n.getKey().equals("0")) {
-                                    mealID = ds.getKey();
-                                    meal = ds.getValue(Meal.class);
-                                    mealList.add(meal);
-                                    listMealID.add(mealID);
-                                }
-                            }
-                        }
-                        chef = ds.getValue(Chef.class);
-                    }
-                }
-                searchResultsListView.setAdapter(mealSearchAdapter);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 
 
@@ -139,6 +108,43 @@ public class ClientOrderFood extends AppCompatActivity implements AdapterView.On
     }
 
     private void searchMeals() {
+        searchFilter = searchEditText.getText().toString();
 
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mealList.clear();
+                listMealID.clear();
+                listChefID.clear();
+
+                //
+                if (searchFilter.equals(categories[0])) {
+                    for (DataSnapshot ds : snapshot.getChildren()) {
+                        chef = ds.getValue(Chef.class);
+                        chefID = ds.getKey();
+                        if (ds.child("role").getValue().equals("Chef") && ds.child("suspended").getValue().equals(false)) {
+                            DataSnapshot meals = ds.child("menu").child("meals");
+                            for (DataSnapshot n : meals.getChildren()) {
+                                if (!n.getKey().equals("0")) {
+                                    mealID = ds.getKey();
+                                    meal = ds.getValue(Meal.class);
+                                    mealList.add(meal);
+                                    chefList.add(chef);
+                                    listMealID.add(mealID);
+                                    listChefID.add(chefID);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                searchResultsListView.setAdapter(mealSearchAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
