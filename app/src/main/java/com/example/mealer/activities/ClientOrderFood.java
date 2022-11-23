9230d203ac8ae5d5027d2c37eeef169b6e69fe5b
TheHiberNate <nathan@gawargy.com>
2022-11-23 09:54:25 -0500

    Update ClientOrderFood.java

--- app/src/main/java/com/example/mealer/activities/ClientOrderFood.java
+++ app/src/main/java/com/example/mealer/activities/ClientOrderFood.java
@@ -1,5 +1,6 @@
 package com.example.mealer.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,29 +9,36 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mealer.R;
+import com.example.mealer.structure.Chef;
 import com.example.mealer.structure.Complaint;
 import com.example.mealer.structure.Meal;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class ClientOrderFood extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
     private String[] categories = {"No Filter", "Chef", "Meal", "Vegetarian"};
-    private String searchFilter;
+    private String searchFilterm, mealID;
     private Spinner options;
+    private EditText searchEditText;
     private TextView noMeals;
     private ListView searchResultsListView;
     private Button searchBtn, backBtn;
     private ArrayList<Meal> mealList;
-    private ArrayList<String> mealIDs;
-    private ArrayList<String> chefIDs;
+    private ArrayList<String> ListMealID, ListChefID;
+    private Meal meal;
+    private Chef chef;
     private DatabaseReference reference;
 
 
@@ -59,6 +67,8 @@ public class ClientOrderFood extends AppCompatActivity implements AdapterView.On
         backBtn = findViewById(R.id.backBtn);
         backBtn.setOnClickListener(this);
 
+        searchEditText = findViewById(R.id.NavigatorSearch);
+
         mealList = new ArrayList<>();
         mealIDs = new ArrayList<>();
         chefIDs = new ArrayList<>(); // id for the chefs of the search result meals
@@ -67,13 +77,49 @@ public class ClientOrderFood extends AppCompatActivity implements AdapterView.On
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        searchFilter = categories[position];
+    protected void onStart() {
+        super.onStart();
+        searchFilter = searchEditText.getText().toString();
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mealList.clear();
+                mealIDs.clear();
+                chefIDs.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    if (ds.child("role").getValue().equals("Chef") && ds.child("suspended").getValue().equals(false)) {
+                        // No Filter search criteria
+                        if (searchFilter.equals(categories[0])) {
+                            DataSnapshot meals = ds.child("menu").child("meals");
+                            for (DataSnapshot n : meals.getChildren()) {
+                                if (!n.getKey().equals("0")) {
+                                    mealID = ds.getKey();
+                                    meal = ds.getValue(Meal.class);
+                                    mealList.add(meal);
+                                    ListMealID.add(mealID);
+                                }
+                            }
+                        }
+                        chef = ds.getValue(Chef.class);
+                    }
+                }
+                menuListView.setAdapter(menuAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
+
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-    }
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) { searchFilter = categories[position]; }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) { }
 
     @Override
     public void onClick(View v) {
