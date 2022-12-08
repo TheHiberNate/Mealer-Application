0648e7e4d4bbd0c0ef43c8f13226e5b722ebd219
kenwescott <87779667+kenwescott@users.noreply.github.com>
2022-12-08 10:52:16 -0500

    ui modif

--- app/src/main/java/com/example/mealer/activities/home_page_client.java
+++ app/src/main/java/com/example/mealer/activities/home_page_client.java
@@ -1,19 +1,33 @@
 package com.example.mealer.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.mealer.HomeFragment;
+import com.example.mealer.OrderFragment;
+import com.example.mealer.ProfileFragment;
 import com.example.mealer.R;
+import com.example.mealer.RatingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class home_page_client extends AppCompatActivity implements View.OnClickListener {
     private Button logout, order, myOrders, rateChef;
     private TextView welcome;
     private String welcomeMessage, clientID;
+
+    private BottomNavigationView bottomNavigationView;
+    private HomeFragment homeFragment=new HomeFragment();
+    private OrderFragment orderFragment=new OrderFragment();
+    private ProfileFragment profileFragment=new ProfileFragment();
+    private RatingsFragment ratingsFragment=new RatingsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +52,31 @@ public class home_page_client extends AppCompatActivity implements View.OnClickL
         welcome = findViewById(R.id.textViewWelcomeClient);
         welcome.setText(welcomeMessage);
         clientID = extras.getString("userID");
+
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.account:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                        return true;
+                    case R.id.order:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,orderFragment).commit();
+                        return true;
+                    case R.id.favorite:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,ratingsFragment).commit();
+                        return true;
+
+                }
+                return false;
+            }
+        });
     }
 
     @Override
